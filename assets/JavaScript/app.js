419060866c4a735bb774e4022d015f92fad20deb
Natalie Brickley <natt_rose@yahoo.com>
2019-03-04 19:50:00 -0800

    user can search for gifs

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,4 +1,4 @@
-var topics = ["cats", "spongebob", "the office", "disney", "cute"]
+var topics = ["cats", "spongebob", "the office", "disney", "cute", "golden girls", "dance"]
 
 //Create a button for each element in the topics array
 for (var i = 0; i < topics.length; i++) {
@@ -11,20 +11,28 @@ $(document).on("click", "#topic", function () {
     //create api url
     var topic = $(this).val()
     var apiKey = "6oreJVucamoE8w9pyekj2hZ6vxC4p8C2"
-    var apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + topic + "&limit=10&offset=0&rating=G&lang=en"
+    var apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + topic + "&limit=10&offset=0&lang=en"
     //Make API call
     $.ajax({
         url: apiUrl,
         method: "GET"
     }).then(function (result) {
+        //clear the results div 
+        $("#results").empty();
         //append 10 static non-animated gif images to the page by...
         for (var i = 0; i < 10; i++) {
+            //creating a div
+            var gifDiv = $("<div class='gif'>")
             //...creating an image
-            var still = result.data[i].images["fixed_width_still"].url
-            var animate = result.data[i].images["fixed_width"].url
+            var still = result.data[i].images["fixed_height_still"].url
+            var animate = result.data[i].images["fixed_height"].url
             var image = $("<img>").attr({ "src": still, "data-animate": animate, "data-still": still })
+            //...append the image as well as it's rating
+            var rating = $("<p>")
+            rating.text("Rating: " + result.data[i].rating.toUpperCase())
+            gifDiv.append(image, rating)
             //...appending to the page
-            $("#results").append(image);
+            $("#results").append(gifDiv);
             //...and assigning the gif to be off
             var on = false;
         }
@@ -48,6 +56,23 @@ $(document).on("click", "#topic", function () {
             }
         })
 
+        
+
+        //issues: if a gif is animated, the next gifs will require a double click to animate if the first gif is still running (if a gif has been paused, pausing the next one will require a double click.)
+
     })
 })
+
+//when the submit button is clicked...
+$(document).on("click","#add-gif", function(event){
+    event.preventDefault();
+    //...get the users input
+    var input = $("#search-gifs").val();
+    //add it to our topics array
+    topics.push(input);
+    //make a button for it
+    var btn = $("<button>").text(input).attr({ "id": "topic", "value": input})
+    $("#buttons").append(btn)
+})
+
 
